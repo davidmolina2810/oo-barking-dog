a3995a7fd657758bbc3efc9b493ae52d0b2a2387
davidmolina2810 <davidmolina2810@gmail.com>
2020-09-05 19:18:40 -0500

    created oo dog class with name attribute and dog method

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Dog
+  def inititalize(name) #initialize dog with name
+    @name = name
+  end
+
+  # setter method for dog name
+  def name=(new_name)
+    @name = new_name
+  end
+
+  # getter method for dog name
+  def name 
+    @name
+  end
+
+  # bark method 
+  def bark 
+    puts "woof!"
+  end
+end
